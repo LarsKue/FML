598827c8a131c41ca2bdcca4b6c906632f869347
Lars Kue <lars@kuehmichel.de>
2020-11-22 15:21:35 +0100

    Merge Changes from FML-HandIn

--- create_handin.py
+++ create_handin.py
@@ -1,12 +1,7 @@
 import argparse
 import zipfile
-import subprocess
 import pathlib
 import warnings
-
-
-def is_ascii(s):
-    return all(ord(c) < 128 for c in s)
 
 
 class TeamMember:
@@ -16,6 +11,81 @@ class TeamMember:
 
     def __str__(self):
         return "-".join(self.last_names + self.first_names).lower()
+
+
+def is_ascii(s):
+    return all(ord(c) < 128 for c in s)
+
+
+def yesno(msg, default=None):
+    valid = {
+        # lowercase only
+        "yes": True,
+        "no": False,
+        "y": True,
+        "n": False,
+        "": default,
+    }
+
+    answer = None
+
+    msg += " [y/n]" + (f" (default: {default})" if default else "") + ": "
+
+    while answer is None:
+        answer = input(msg)
+        answer = valid.get(answer)
+
+    return answer
+
+
+def find_notebooks(path: pathlib.Path):
+    # autodetect the notebook files
+    ccommented = set(path.glob("*-cross-commented.ipynb"))
+    commented = set(path.glob("*-commented.ipynb")) - ccommented
+    notebook = set(path.glob("*.ipynb")) - commented - ccommented
+
+    # exception handling
+    if len(notebook) > 1:
+        raise RuntimeError(f"Found more than one main notebook in {path}")
+
+    if not notebook:
+        raise FileNotFoundError(f"Found no main notebook in {path}")
+    else:
+        notebook = notebook.pop()
+        print(f"Found main notebook {notebook}")
+
+    if len(commented) > 1:
+        raise RuntimeError(f"Found more than one commented notebook in {path}")
+
+    if not commented:
+        warnings.warn(f"There is no commented notebook in {path}.")
+        if not yesno(f"Are you sure you want to continue?"):
+            raise FileNotFoundError
+
+        commented = None
+    else:
+        commented = commented.pop()
+        print(f"Found commented notebook {commented}")
+
+    if len(ccommented) > 1:
+        raise RuntimeError(f"Found more than one cross-commented notebook in {path}")
+
+    if not ccommented:
+        ccommented = None
+    else:
+        ccommented = ccommented.pop()
+        print(f"Found cross-commented notebook {ccommented}")
+
+    # return all available notebooks
+    return notebook, commented, ccommented
+
+
+def convert_ipynb_html(path):
+    import subprocess
+    # we use jupyter for the conversion
+    subprocess.run(f"jupyter nbconvert --to html {path.absolute()}")
+    # return the path to the new html file
+    return path.with_suffix(".html")
 
 
 def main():
@@ -35,34 +105,39 @@ def main():
     parser = argparse.ArgumentParser(description="Automatically create the hand-in file for the lecture FML.")
     parser.add_argument("name", type=str,
                         help="The name of the exercise, to be included at the end of the zip filename.")
-    parser.add_argument("notebook", type=str, help="The name of the Jupyter Notebook")
     parser.add_argument("-m", "--more", type=str,
                         help="Glob Pattern to determine additional files which should be added to the Hand-In.")
 
     args = parser.parse_args()
 
-    # check if directory and file exist
+    # check if directory exists
     ex_path = pathlib.Path(args.name)
     if not ex_path.is_dir():
         raise ValueError(f"Exercise Path '{args.name}' does not exist.")
-    nb_file = ex_path / (args.notebook + ".ipynb")
 
-    if not nb_file.is_file():
-        raise ValueError(f"Notebook '{args.notebook}.ipynb' does not exist in '{args.name}'.")
-
-    # both exist, create an html file
-    subprocess.run(f"jupyter nbconvert --to html {nb_file.absolute()}")
-
-    html_file = ex_path / (args.notebook + ".html")
+    # first check for notebooks before creating zip file
+    nb, com, ccom = find_notebooks(ex_path)
 
     zip_path = ex_path / (members + "_" + args.name + ".zip")
 
     # create the zip hand-in file
     zipf = zipfile.ZipFile(zip_path, "w")
 
+    # notebook and directory are fine, convert the notebook to html
+    nb_html = convert_ipynb_html(nb)
     # add the html and notebook to the zip
-    zipf.write(html_file, html_file.name)
-    zipf.write(nb_file, nb_file.name)
+    zipf.write(nb, nb.name)
+    zipf.write(nb_html, nb_html.name)
+
+    if com:
+        com_html = convert_ipynb_html(com)
+        zipf.write(com, com.name)
+        zipf.write(com_html, com_html.name)
+
+    if ccom:
+        ccom_html = convert_ipynb_html(ccom)
+        zipf.write(ccom, ccom.name)
+        zipf.write(ccom_html, ccom_html.name)
 
     if args.more:
         files = list(ex_path.glob(args.more))
